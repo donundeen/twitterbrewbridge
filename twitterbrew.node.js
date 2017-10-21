72c2202f95018ddff60516e4672d7b70c58aa70a
don undeen <donundeen@yahoo.com>
2017-10-21 18:18:08 -0400

    outlined in pseudocode

---
+++ twitterbrew.node.js
@@ -0,0 +1,65 @@
+/*
+
+subscribers -> Spacebrew -> Twitter
+
+Twitter -> Spacebrew -> Publishers
+
+
+twitter user = Spacebrew instance
+twitter hashtag = spacebrew publisher/subscriber
+
+Type: string
+
+bridgeconfig
+{
+twitteruser1 : {
+  pubsub1,
+  pubsub2
+
+},
+
+twitteruser2 : {
+  pubsub3,
+  pubsub4,
+
+}
+
+}
+
+sbs = {};
+twts = {};
+
+each(bridgeconfigs as user, config){
+  sbs[user] = new spacebrew();
+  twts[user] = new twitter(user);
+  
+  each(config as channel){
+    sbs[user].addpublish(channel, "string");
+    sbs[user].addsubscribe(channel, "string");
+    function(_user, _channel){
+      sbs[_user].handlestring(string, function(){
+        twts[_user].dms.sendmsgs("#"+_channel+")" ::: " + string); // send as dm (or with @ ?)
+      });
+    }(_user, channel);
+  }
+}
+
+
+every(second){
+  each(configs as user, config){
+    function(_user){
+      each(config as channel){
+        twts[_user].dms().search("#"+_channel+")" ::: ", fuction(results){ // searching dms, or @?
+          each(results as result){
+            var message = result.match(/regex/)[1];
+            sbs[user].sendmsg(_channel, message);
+          }
+        });
+    }(user);
+  }
+}
+*/
+
+
+
+
